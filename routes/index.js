806f9038f00f2ef9fc58ad9f8fdf99b8c44e004f
Marlon Kuzmick <mkuzmick@gmail.com>
2018-09-18 07:35:49 -0400

    menu route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: "what's cooking" });
 });
 
+router.get('/menu', function(req, res, next) {
+  // get appts from google cal?
+  // get 
+  res.render('menu', { title: "what's cooking" });
+});
+
+
 module.exports = router;
